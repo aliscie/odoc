f36a92494485d8a54908ad7920c1812fe8eadec5
aliscie <alihushamsci@icloud.com>
2023-05-29 11:58:00 +0300

    unit test

--- frontend/tests/App.test.tsx
+++ frontend/tests/App.test.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import BasicMenu from "../components/genral/basic_menu";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+describe('BasicMenu', () => {
+  test('renders menu options and triggers onClick', () => {
+    const mockOnClick = jest.fn();
+
+    const options = [
+      { content: 'option 1', onClick: mockOnClick },
+      { content: 'option 2' },
+    ];
+
+    render(
+      <BasicMenu options={options}>
+        <button>Open Menu</button>
+      </BasicMenu>
+    );
+
+    const openMenuButton = screen.getByText('Open Menu');
+    fireEvent.click(openMenuButton);
+
+    const option1 = screen.getByText('option 1');
+    const option2 = screen.getByText('option 2');
+
+    fireEvent.click(option1);
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+
+    fireEvent.click(option2);
+    expect(mockOnClick).toHaveBeenCalledTimes(1); // onClick should not be triggered for option2
+  });
 });
