c63b40ad7ef8ddf3a54354b6a348f084b2cb5e56
aliscie <alihushamsci@icloud.com>
2023-12-22 16:11:47 +0300

    In order to help users to keep track if the files shared from others, We can store the shared files and retrieve a list of them.

--- src/user_canister/src/share_files/queries.rs
+++ src/user_canister/src/share_files/queries.rs
@@ -1,7 +1,7 @@
 use ic_cdk_macros::query;
 use crate::files::FileNode;
 
-use crate::ShareFile;
+use crate::{SHARED_USER_FILES, ShareFile};
 use crate::storage_schema::ContentTree;
 
 // TODO Note here should be only queries not updates.
@@ -12,6 +12,26 @@ fn get_shared_file(share_id: String) -> Result<(FileNode, ContentTree), String> 
     // Ok((file, content_tree))
     ShareFile::get_file(&share_id)
 }
+
+#[query]
+fn get_shared_files() -> Result<Vec<(FileNode, ContentTree)>, String> {
+    SHARED_USER_FILES.with(|files| {
+        let files = files.borrow();
+        let files: Vec<(FileNode, ContentTree)> = files
+            .iter()
+            .map(|(_, share)| {
+                let res: Result<(FileNode, ContentTree), String> = ShareFile::get_file(&share.id);
+                if let Ok(res) = res {
+                    res
+                } else {
+                    (FileNode::new("NowAllowed".to_string(), None), ContentTree::default())
+                }
+            })
+            .collect();
+        Ok(files)
+    })
+}
+
 
 #[query]
 fn get_share_file(share_id: String) -> Result<ShareFile, String> {
--- src/user_canister/src/share_files/updates.rs
+++ src/user_canister/src/share_files/updates.rs
@@ -1,10 +1,23 @@
-
+use ic_cdk::caller;
 use ic_cdk_macros::update;
 
-use crate::ShareFile;
+use crate::{SHARED_USER_FILES, ShareFile};
 
 #[update]
 fn share_file(new_share_file: ShareFile) -> Result<ShareFile, String> {
     new_share_file.save()
     // ShareFile::new(file_id, share_id)
+}
+
+
+#[update]
+fn add_to_my_files(share_file: ShareFile) -> Result<ShareFile, String> {
+    if caller().to_string() == share_file.owner.to_string() {
+        return Err("You can't add your own file.".to_string());
+    }
+    SHARED_USER_FILES.with(|files| {
+        let mut files = files.borrow_mut();
+        files.insert(share_file.id.clone(), share_file.clone());
+        Ok(share_file)
+    })
 }
