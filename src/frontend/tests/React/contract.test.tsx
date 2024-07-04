b6840e912331e6acfbb8afb9bb670715e5751dc2
aliscie <alihushamsci@icloud.com>
2024-07-04 10:19:20 +0300

    fix middleware issue with hummocked testing

--- src/frontend/tests/React/contract.test.tsx
+++ src/frontend/tests/React/contract.test.tsx
@@ -1,36 +1,31 @@
 import React from 'react';
+import {render} from '@testing-library/react';
+import {Principal} from '@dfinity/principal';
+import {randomString} from '../../data_processing/data_samples';
+import {CustomContract} from '../../../declarations/backend/backend.did';
+import CustomContractComponent from '../../components/contracts/custom_contract/custom_contract';
 import TestWrapper from "../utls/tests_wrapper";
-import { render, fireEvent, screen } from '@testing-library/react';
-import {Principal} from "@dfinity/principal";
-import {randomString} from "../../data_processing/data_samples";
-import {CustomContract} from "../../../declarations/backend/backend.did";
-import {CustomContractComponent} from "../../components/contracts/custom_contract/custom_contract";
-// import {custom_contract} from "../../data_processing/data_samples";
 
 const custom_contract: CustomContract = {
-    'id': randomString(),
-    "name": "Custom contract",
-    'creator': Principal.fromText("2vxsx-fae"),
-    'date_created': Date.now() * 1e6,
-    'payments': [],
-    'promises': [],
-    'contracts': [],
-    'formulas': [],
-    'date_updated': 0,
-    'permissions': [],
-}
-
+    id: randomString(),
+    name: "Custom contract",
+    creator: Principal.fromText("2vxsx-fae"),
+    date_created: Date.now() * 1e6,
+    payments: [],
+    promises: [],
+    contracts: [],
+    formulas: [],
+    date_updated: 0,
+    permissions: [],
+};
 
 it('creates and updates a contract, then interacts with rows and columns', () => {
-
-
-    const {getByText} = render(
+    const { getByText } = render(
         <TestWrapper>
-            <CustomContractComponent contract={custom_contract}/>
+            <CustomContractComponent contract={custom_contract} />
         </TestWrapper>
     );
 
+    // Add your test expectations here
     // expect(getByText("hello world")).toBeInTheDocument();
 });
-
-
