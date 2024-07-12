76dcde05edb9d49c5fdadaa8d5a6d6e590681e1c
koladeolukoya <koladeolukoya@gmail.com>
2024-07-12 12:26:51 +0100

    feat: improvement on side nav bar

--- src/frontend/components/spesific/nav_bar.tsx
+++ src/frontend/components/spesific/nav_bar.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import "./style/nav_bar.css";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import NestedList from "./nested_list/nest_list";
-import {Button, Divider, Drawer, List, ListItemButton, ListItemText} from "@mui/material";
-import {Link} from "react-router-dom";
+import { Button, Divider, Drawer, List, ListItemButton, ListItemText } from "@mui/material";
+import { Link } from "react-router-dom";
 import CreateFile from "../actions/create_file";
-import {handleRedux} from "../../redux/main";
+import { handleRedux } from "../../redux/main";
+import InfoIcon from '@mui/icons-material/Info';
+import ExploreIcon from '@mui/icons-material/Explore';
 
 const NavBar = (props: any) => {
     const dispatch = useDispatch();
-    const {isNavOpen, isLoggedIn} = useSelector((state: any) => state.uiReducer);
-    const {files} = useSelector((state: any) => state.filesReducer);
+    const { isNavOpen, isLoggedIn } = useSelector((state: any) => state.uiReducer);
+    const { files } = useSelector((state: any) => state.filesReducer);
 
     const navLinks = [
-        {label: "Discover", to: "/discover"},
-        {label: "About us", to: "/"},
+        { label: "About Us", to: "/", icon: <InfoIcon /> },
+        { label: "Discover", to: "/discover", icon: <ExploreIcon /> },
     ];
 
     return (
@@ -25,31 +27,41 @@ const NavBar = (props: any) => {
                 anchor="left"
                 PaperProps={{
                     className: "sidenav card bg-blur",
-                    style: {width: "250px"},
+                    style: { width: "250px", padding: "16px", paddingTop: "80px" },
                 }}
             >
                 <List>
                     {navLinks.map((link) => (
                         <ListItemButton
+                            key={link.label}
                             onClick={() => {
-                                // TODO find a genaric way to hid the <CopyButton/> (Share Button) when window.location.pathname not a file id.
-                                dispatch(handleRedux("CURRENT_FILE", {file: null}));
+                                dispatch(handleRedux("CURRENT_FILE", { file: null }));
                             }}
-                            component={Link} to={link.to} key={link.label}>
-                            <ListItemText primary={link.label}/>
+                            component={Link}
+                            to={link.to}
+                            sx={{
+                                margin: "8px 0",
+                                borderRadius: "8px",
+                                '&:hover': {
+                                    backgroundColor: "#e0e0e0",
+                                }
+                            }}
+                        >
+                            {link.icon}
+                            <ListItemText primary={link.label} sx={{ marginLeft: "16px" }} />
                         </ListItemButton>
                     ))}
                 </List>
-                <Divider/>
+                <Divider />
                 {isLoggedIn && (
                     <>
-                        <NestedList files={files}/>
-                        <Divider/>
-                        <CreateFile/>
+                        <NestedList files={files} />
+                        <Divider />
+                        <CreateFile />
                     </>
                 )}
             </Drawer>
-            <div id="main" style={{marginLeft: isNavOpen ? "250px" : 0}}>
+            <div id="main" style={{ marginLeft: isNavOpen ? "250px" : 0 }}>
                 <span>{props.children}</span>
             </div>
         </div>
