0a770aca41ae08248150b578daed60a3657dcf40
koladeolukoya <koladeolukoya@gmail.com>
2024-08-19 12:07:02 +0100

    refactor: deposit component with type assertion

--- src/frontend/pages/profile/actions/deposit.tsx
+++ src/frontend/pages/profile/actions/deposit.tsx
@@ -4,18 +4,23 @@ import LoaderButton from "../../../components/MuiComponents/LoaderButton";
 import { handleRedux } from "../../../redux/store/handleRedux";
 import { MonetizationOn } from "@mui/icons-material";
 import { useBackendContext } from "../../../contexts/BackendContext";
+import { Result_3 } from "../../../../declarations/backend/backend.did";
 
-function Deposit() {
+const Deposit = () => {
     const { backendActor } = useBackendContext();
     const dispatch = useDispatch();
 
     const handleDeposit = async () => {
         if (!backendActor) return;
 
-        const res = await backendActor.deposit_usdt(100);
-        if (res?.Ok) {
+        const res = await backendActor.deposit_usdt(100) as Result_3; // Result_3 represent the return type of the function
+
+        if ("Ok" in res) {
             dispatch(handleRedux("UPDATE_BALANCE", { balance: res.Ok }));
+        } else if ("Err" in res) {
+            console.error(`Deposit failed: ${res.Err}`);
         }
+
         return res;
     };
 
