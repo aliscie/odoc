5ec94a39199decd9fbc5d9e9c4a64e24f372d138
koladeolukoya <koladeolukoya@gmail.com>
2024-08-22 06:52:29 +0100

    ref: list item component

--- src/frontend/components/MainComponents/NestedList/ListItem.tsx
+++ src/frontend/components/MainComponents/NestedList/ListItem.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import Collapse from "@mui/material/Collapse";
 import List from "@mui/material/List";
-import {NestedDataItem} from "./nestList";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { handleRedux } from "../../../redux/store/handleRedux";
 import DeleteFile from "../../Actions/DeleteFile";
 import Draggable from "../../MuiComponents/Draggable";
 import ShareIcon from '@mui/icons-material/Share';
 import ChangeWorkSpace from "../../Actions/ChangeWorkSpaceFile";
 import ContextMenu from "../../MuiComponents/ContextMenu";
+import { NestedDataItem } from "./nestList";
 
 interface ItemProps {
-    data: Record<number, NestedDataItem>; // Use Record<number, NestedDataItem> instead of any
+    data: Record<number, NestedDataItem>;
     item: NestedDataItem;
     index: number;
     openItems: number[];
@@ -24,69 +24,92 @@ interface ItemProps {
     isChild?: boolean;
 }
 
-const DocComponent: React.FC<ItemProps> = ({data, item, index, openItems, handleClick, path = null, pl = 1}) => {
-    const {profile} = useSelector((state: any) => state.filesState);
+const DocComponent: React.FC<ItemProps> = ({
+    data,
+    item,
+    index,
+    openItems,
+    handleClick,
+    path = null,
+    pl = 1,
+}) => {
+    // Get the user's profile from the Redux store
+    const { profile } = useSelector((state: any) => state.filesState);
 
     const dispatch = useDispatch();
 
-    const html_file_id = `file${item.id}`;
+    // Define the ID for the HTML element representing this file
+    const htmlFileId = `file${item.id}`;
     const isOpen = openItems.includes(index);
     const hasChildren = item.children.length > 0;
 
+    // Handle item click event
     const handleItemClick = () => {
         handleClick(index);
-        dispatch(handleRedux("CURRENT_FILE", {file: item}));
+        dispatch(handleRedux("CURRENT_FILE", { file: item }));
     };
 
-    path = path ? path : item.id;
-    path = path && path.replace(/\s+/g, '_').toLowerCase();
-    path = path && path.replaceAll(".", "")
+    // Construct the path for the Link component
+    let itemPath = path ? path : item.id.toString();
+    itemPath = itemPath.replace(/\s+/g, '_').toLowerCase().replaceAll(".", "");
 
-    let options = [
-        // {
-        //     content: <RenameFile item={item}/>,
-        //     preventClose: true,
-        // },
-        {content: <DeleteFile item={item}/>, preventClose: true,},
-        {content: <ChangeWorkSpace item={item}/>},
-    ]
+    // Context menu options for the item
+    const contextMenuOptions = [
+        { content: <DeleteFile item={item} />, preventClose: true },
+        { content: <ChangeWorkSpace item={item} /> },
+    ];
 
-    const handleDrop = async ({draggedId, targetId, dragOverPosition, type, index, clientY}) => {
+    // Handle drop event for draggable items
+    const handleDrop = async ({
+        draggedId,
+        targetId,
+        dragOverPosition,
+        index,
+    }) => {
+        const dragged = data[draggedId];
+        const target = data[targetId];
 
-        let dragged = data.find(f => f.id == draggedId);
-        let target = data.find(f => f.id == targetId);
-        // logger({dragged, target, dragOverPosition, type, index, clientY});
-        let parent = [targetId]
-        if (dragOverPosition == 'under') {
-            parent = target.parent
-        } else if (dragOverPosition == 'above') {
-            parent = target.parent
+        let parent = [targetId];
+        if (dragOverPosition === "under") {
+            parent = target.parent;
+        } else if (dragOverPosition === "above") {
+            parent = target.parent;
             index--;
         }
+
         dispatch(handleRedux("CHANGE_FILE_PARENT", {
             position: dragOverPosition,
             id: draggedId,
             parent,
-            index: index
+            index,
         }));
     };
 
-
     return (
         <>
-            <Link to={path}>
-                <ContextMenu options={options}>
+            <Link to={itemPath}>
+                <ContextMenu options={contextMenuOptions}>
                     <Draggable
                         index={index}
                         id={item.id}
                         onDrop={handleDrop}
                     >
                         <ListItemButton
-                            id={html_file_id} onClick={handleItemClick} sx={{pl}}>
-                            {hasChildren && (isOpen ? <ExpandLess/> : <ExpandMore/>)}
-                            <ListItemText primary={<>
-                                {item.name} {item.author != profile.id && <ShareIcon size={"small"}/>}
-                            </>}/>
+                            id={htmlFileId}
+                            onClick={handleItemClick}
+                            sx={{ pl }}
+                        >
+                            {hasChildren && (isOpen ? <ExpandLess /> : <ExpandMore />)}
+                            <ListItemText
+                                primary={
+                                    <>
+                                        {item.name}
+                                        {item.author !== profile.id && (
+                                            <ShareIcon fontSize="small" />
+                                        )}
+                                    </>
+                                }
+                            />
                         </ListItemButton>
                     </Draggable>
                 </ContextMenu>
@@ -94,19 +117,19 @@ const DocComponent: React.FC<ItemProps> = ({data, item, index, openItems, handle
             {hasChildren && (
                 <Collapse in={isOpen} timeout="auto" unmountOnExit>
                     <List component="div" disablePadding>
-                        {[...item.children].map((childId, childIndex) => {
-                            const childItem = data.find(f => f.id == childId)
+                        {item.children.map((childId, childIndex) => {
+                            const childItem = data[childId];
                             if (childItem) {
                                 return (
                                     <DocComponent
-                                        path={path + "/" + childItem.id}
+                                        path={`${itemPath}/${childItem.id}`}
                                         key={childItem.id}
                                         data={data}
                                         item={childItem}
                                         index={index + childIndex + 1}
                                         openItems={openItems}
                                         handleClick={handleClick}
-                                        pl={pl + 3} // Pass the isChild prop to child items
+                                        pl={pl + 3}
                                     />
                                 );
                             }
@@ -118,4 +141,5 @@ const DocComponent: React.FC<ItemProps> = ({data, item, index, openItems, handle
         </>
     );
 };
+
 export default DocComponent;
