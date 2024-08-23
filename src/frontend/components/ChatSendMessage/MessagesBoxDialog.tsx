1a6b296657d87cb4eb3c9ea00610e027eff045fe
koladeolukoya <koladeolukoya@gmail.com>
2024-08-23 05:37:16 +0100

    clean: message dialog

--- src/frontend/components/ChatSendMessage/MessagesBoxDialog.tsx
+++ src/frontend/components/ChatSendMessage/MessagesBoxDialog.tsx
@@ -1,34 +1,53 @@
 import React, { useState } from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useSnackbar } from 'notistack';
 import { handleRedux } from '../../redux/store/handleRedux';
 import MessagesList from '../ChatNotifications/MessagesList';
-import MessageInput from './MessageInput';
 import MessageDialog from '../MuiComponents/MessageDialog';
 import { IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 import SendMessageBox from './SendMessageBox';
 
-const MessagesDialogBox = () => {
+interface Message {
+    text: string;
+    isCurrentUser: boolean;
+}
+
+const MessagesDialogBox: React.FC = () => {
     const dispatch = useDispatch();
     const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-    const { current_chat_id } = useSelector((state:any) => state.chatsState)
-    const [messages, setMessages] = useState([]);
-    const is_path_chats = window.location.pathname.includes('/chats');
+    const { current_chat_id } = useSelector((state: any) => state.chatsState);
+    
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const isPathChats = window.location.pathname.includes('/chats');
+    const isOpen = !isPathChats && Boolean(current_chat_id);
 
     const closeDialog = () => {
-        dispatch(handleRedux('OPEN_CHAT', { current_chat_id: false }));
+        try {
+            dispatch(handleRedux('OPEN_CHAT', { current_chat_id: false }));
+        } catch (error) {
+            enqueueSnackbar('Failed to close the dialog', { variant: 'error' });
+            console.error('Error closing dialog:', error);
+        }
     };
 
-    const handleSend = (message) => {
-        setMessages([...messages, { text: message, isCurrentUser: true }]);
+    const handleSend = (message: string) => {
+        if (message.trim()) {
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                { text: message, isCurrentUser: true }
+            ]);
+        } else {
+            enqueueSnackbar('Message cannot be empty', { variant: 'warning' });
+        }
     };
 
     return (
         <MessageDialog
-            open={!is_path_chats && current_chat_id ? true : false}
+            open={isOpen}
             title="Messages"
             inputFields={<MessagesList messages={messages} />}
             actions={[
