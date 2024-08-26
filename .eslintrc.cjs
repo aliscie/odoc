3302c485bb04c2255edd769a301da45fc70fdb48
koladeolukoya <koladeolukoya@gmail.com>
2024-08-26 08:37:22 +0100

    fmt: prettier configuration done

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,25 @@
 module.exports = {
     root: true,
-    env: {browser: true, es2020: true},
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+    env: { browser: true, es2020: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:storybook/recommended',
+      'plugin:prettier/recommended', // Add this line
+    ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
+    plugins: ['react-refresh', 'prettier'], // Add 'prettier' here
     rules: {
-        "react/react-in-jsx-scope": "off",
-        "unused-imports/no-unused-imports": "error",
-        "unused-imports/no-unused-vars": "warn",
-        '@typescript-eslint/no-unused-vars': ['warn', {'varsIgnorePattern': '^_', 'argsIgnorePattern': '^_'}],
-        'react-refresh/only-export-components': [
-            'warn',
-            {allowConstantExport: true},
-        ],
+      "react/react-in-jsx-scope": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": "warn",
+      '@typescript-eslint/no-unused-vars': ['warn', { 'varsIgnorePattern': '^_', 'argsIgnorePattern': '^_' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error', // Add this line
     },
-}
+  };
