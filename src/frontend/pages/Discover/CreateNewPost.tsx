4daa65cc6c0a8e8196f0bf73465d34d182730e4f
koladeolukoya <koladeolukoya@gmail.com>
2024-08-27 06:33:26 +0100

    style: discover page ui

--- src/frontend/pages/Discover/CreateNewPost.tsx
+++ src/frontend/pages/Discover/CreateNewPost.tsx
@@ -1,121 +1,121 @@
-import {Divider, Grid} from "@mui/material";
+import { Divider, Grid } from "@mui/material";
 import React from "react";
 import IconButton from "@mui/material/IconButton";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import MoreTimeIcon from "@mui/icons-material/MoreTime";
-import {ContentNode, Post} from "../../../declarations/backend/backend.did";
-import {randomId} from "@mui/x-data-grid-generator";
-import {useSelector} from "react-redux";
-import {useSnackbar} from "notistack";
-import {LoadingButton} from "@mui/lab";
+import { ContentNode, Post } from "../../../declarations/backend/backend.did";
+import { randomId } from "@mui/x-data-grid-generator";
+import { useSelector } from "react-redux";
+import { useSnackbar } from "notistack";
+import { LoadingButton } from "@mui/lab";
 import PostComponent from "../../components/MuiComponents/PostComponent";
-import {useBackendContext} from "../../contexts/BackendContext";
+import { useBackendContext } from "../../contexts/BackendContext";
 import PostTags from "./TagsComponent";
 import serializeFileContents from "../../DataProcessing/serialize/serializeFileContents";
 
 function CreatePost(props: any) {
-    let post_id = randomId();
-    const {profile} = useSelector((state: any) => state.filesState);
-    const [post, setPost] = React.useState<Post>({
-        id: post_id,
-        creator: profile ? profile.id : "",
-        date_created: BigInt(0),
-        votes_up: [],
-        tags: [],
-        content_tree: [],
-        votes_down: [],
-    });
+  let post_id = randomId();
+  const { profile } = useSelector((state: any) => state.filesState);
+  const [post, setPost] = React.useState<Post>({
+    id: post_id,
+    creator: profile ? profile.id : "",
+    date_created: BigInt(0),
+    votes_up: [],
+    tags: [],
+    content_tree: [],
+    votes_down: [],
+  });
 
-    const {enqueueSnackbar, closeSnackbar} = useSnackbar();
-    let [isEnter, setEnter] = React.useState(false);
-    let [loading, setLoad] = React.useState(false);
-    let [changes, setChanges] = React.useState<any>(null);
-    const {backendActor} = useBackendContext();
-    const handleCreatePost = async () => {
-
-        let de_changes: Array<Array<[string, Array<[string, ContentNode]>]>> = serializeFileContents(changes);
-        let content_tree: Array<[string, ContentNode]> = de_changes[0][0][1];
-        let new_post = {...post, content_tree: content_tree};
-        setLoad(true);
-        let res = await backendActor?.save_post(new_post);
-        console.log({res})
-        setLoad(false);
-        if ("Ok" in res) {
-            // TODo Why new posts does not show up
-            // props.setPosts((pre) => {
-            //     let new_posts = [];
-            //     if (pre.length > 0) {
-            //         new_posts = pre;
-            //     }
-            //     new_post = {
-            //         ...new_post,
-            //         creator: {name: profile.name, id: profile.id},
-            //     };
-            //     return [new_post, ...new_posts];
-            // });
-            // enqueueSnackbar("Post created", {variant: "success"});
-            // setChanges(null);
-        } else {
-            enqueueSnackbar("Error creating post. " + res.Err, {variant: "error"});
-        }
-    };
-    let CreateButtons = (props: any) => (
-        <>
-            <LoadingButton
-                // disabled={loading}
-                loading={loading}
-                onClick={handleCreatePost}
-            >
-                <AddCircleOutlineIcon/>
-            </LoadingButton>
-            <IconButton>
-                <MoreTimeIcon/>
-            </IconButton>
-            <PostTags
-                post={post}
-                setTags={(updatedTags) => {
-                    setPost((prevPost) => ({
-                        ...prevPost,
-                        tags: updatedTags.map((tag) => tag.title),
-                    }));
-                }}
-            />
-        </>
-    );
-
-    function onChange(changes: any) {
-        let new_change = {};
-        new_change[""] = changes;
-        setChanges(new_change);
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  let [isEnter, setEnter] = React.useState(false);
+  let [loading, setLoad] = React.useState(false);
+  let [changes, setChanges] = React.useState<any>(null);
+  const { backendActor } = useBackendContext();
+  const handleCreatePost = async () => {
+    let de_changes: Array<Array<[string, Array<[string, ContentNode]>]>> =
+      serializeFileContents(changes);
+    let content_tree: Array<[string, ContentNode]> = de_changes[0][0][1];
+    let new_post = { ...post, content_tree: content_tree };
+    setLoad(true);
+    let res = await backendActor?.save_post(new_post);
+    console.log({ res });
+    setLoad(false);
+    if ("Ok" in res) {
+      // TODo Why new posts does not show up
+      // props.setPosts((pre) => {
+      //     let new_posts = [];
+      //     if (pre.length > 0) {
+      //         new_posts = pre;
+      //     }
+      //     new_post = {
+      //         ...new_post,
+      //         creator: {name: profile.name, id: profile.id},
+      //     };
+      //     return [new_post, ...new_posts];
+      // });
+      // enqueueSnackbar("Post created", {variant: "success"});
+      // setChanges(null);
+    } else {
+      enqueueSnackbar("Error creating post. " + res.Err, { variant: "error" });
     }
+  };
+  let CreateButtons = (props: any) => (
+    <>
+      <LoadingButton
+        // disabled={loading}
+        loading={loading}
+        onClick={handleCreatePost}
+      >
+        <AddCircleOutlineIcon />
+      </LoadingButton>
+      <IconButton>
+        <MoreTimeIcon />
+      </IconButton>
+      <PostTags
+        post={post}
+        setTags={(updatedTags) => {
+          setPost((prevPost) => ({
+            ...prevPost,
+            tags: updatedTags.map((tag) => tag.title),
+          }));
+        }}
+      />
+    </>
+  );
 
-    return (
-        <Grid
-            onMouseEnter={() => setEnter(true)}
-            onMouseLeave={() => setEnter(false)}
-            item
-            sx={{
-                my: 1,
-                mx: "auto",
-                p: 2,
-                // marginLeft: '20%',
-                // marginRight: '20%',
-                // marginBottom: '20%',
-                opacity: isEnter ? 1 : 0.2,
-                // height: '10%', // adjust the value accordingly
-            }}
-        >
-            <PostComponent
-                is_owner={true}
-                s
-                key={changes} // Use the key to force a re-render
-                buttons={<CreateButtons/>}
-                post={post}
-                onChange={onChange}
-            />
-            <Divider/>
-        </Grid>
-    );
+  function onChange(changes: any) {
+    let new_change = {};
+    new_change[""] = changes;
+    setChanges(new_change);
+  }
+
+  return (
+    <Grid
+      onMouseEnter={() => setEnter(true)}
+      onMouseLeave={() => setEnter(false)}
+      item
+      sx={{
+        my: 1,
+        mx: "auto",
+        p: 2,
+        // marginLeft: '20%',
+        // marginRight: '20%',
+        // marginBottom: '20%',
+        opacity: isEnter ? 1 : 0.2,
+        // height: '10%', // adjust the value accordingly
+      }}
+    >
+      <PostComponent
+        is_owner={true}
+        s
+        key={changes} // Use the key to force a re-render
+        buttons={<CreateButtons />}
+        post={post}
+        onChange={onChange}
+      />
+      <Divider />
+    </Grid>
+  );
 }
 
 export default CreatePost;
--- src/frontend/pages/Discover/index.tsx
+++ src/frontend/pages/Discover/index.tsx
@@ -1,108 +1,108 @@
-import React, {useEffect} from 'react';
-import '../styles/LandingPage.css';
-import {Button, Divider, Grid} from "@mui/material";
-import {useSelector} from "react-redux";
+import React, { useEffect } from "react";
+import "../styles/LandingPage.css";
+import { Button, Divider, Grid } from "@mui/material";
+import { useSelector } from "react-redux";
 import CreatePost from "./CreateNewPost";
-import {PostUser} from "../../../declarations/backend/backend.did";
-import {useSnackbar} from "notistack";
+import { PostUser } from "../../../declarations/backend/backend.did";
+import { useSnackbar } from "notistack";
 import FilterPosts from "./PostsFilters";
 import ViewPost from "./ViewUpdatePost";
-import {useBackendContext} from "../../contexts/BackendContext";
+import { useBackendContext } from "../../contexts/BackendContext";
+import Box from "@mui/material/Box";
 
 const Discover = () => {
-        const {backendActor} = useBackendContext();
-        const {searchValue} = useSelector((state: any) => state.uiState);
-        const {isLoggedIn} = useSelector((state: any) => state.uiState);
+  const { backendActor } = useBackendContext();
+  const { searchValue } = useSelector((state: any) => state.uiState);
+  const { isLoggedIn } = useSelector((state: any) => state.uiState);
 
-        const [posts, setPosts] = React.useState<| Array<PostUser>>([]); //TODO use redux for this
-        const [current_page, setPage] = React.useState<number>(0);
-        const {enqueueSnackbar} = useSnackbar();
-        useEffect(() => {
-            let timeoutId: NodeJS.Timeout;
+  const [posts, setPosts] = React.useState<Array<PostUser>>([]); //TODO use redux for this
+  const [current_page, setPage] = React.useState<number>(0);
+  const { enqueueSnackbar } = useSnackbar();
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
 
-            const delayedSearch = async () => {
-                // TODO later add a Button for deep search_popper in cuz query can cost cycles.
-                if (searchValue.length > 0) {
-                    let res: Array<PostUser> = await backendActor.search_posts(searchValue);
-                    res && setPosts(res);
-                } else {
-                    setPage(0)
-                    setPosts([])
-                    await set_posts()
-                }
+    const delayedSearch = async () => {
+      // TODO later add a Button for deep search_popper in cuz query can cost cycles.
+      if (searchValue.length > 0) {
+        let res: Array<PostUser> = await backendActor.search_posts(searchValue);
+        res && setPosts(res);
+      } else {
+        setPage(0);
+        setPosts([]);
+        await set_posts();
+      }
+    };
 
-            };
+    // Clear the previous timeout
+    clearTimeout(timeoutId);
 
-            // Clear the previous timeout
-            clearTimeout(timeoutId);
+    // Set a new timeout
+    timeoutId = setTimeout(delayedSearch, 300);
 
-            // Set a new timeout
-            timeoutId = setTimeout(delayedSearch, 300);
+    // Cleanup function
+    return () => clearTimeout(timeoutId);
+  }, [searchValue]);
 
-            // Cleanup function
-            return () => clearTimeout(timeoutId);
-        }, [searchValue]);
+  async function set_posts() {
+    posts.length > 0 && setPage(posts.length);
+    let res: Array<PostUser> = await backendActor.get_posts(
+      BigInt(current_page),
+      BigInt(current_page + 10),
+    );
 
-
-        async function set_posts() {
-
-
-            posts.length > 0 && setPage(posts.length);
-            let res: Array<PostUser> = await backendActor.get_posts(BigInt(current_page), BigInt(current_page + 10))
-
-            if (res && res.length > 0) {
-                setPosts((pre) => {
-                    return posts.length == 0 ? [...res] : [...pre, ...res]
-                })
-            } else if (res && res.length == 0) {
-                enqueueSnackbar("There are no more posts to load.", {variant: "info"})
-            } else {
-                enqueueSnackbar("undefined Error getting posts.", {variant: "error"})
-            }
-        }
-
-        useEffect(() => {
-
-            (async () => {
-                await set_posts()
-            })()
-        }, [])
-
-        return (
-            <Grid
-
-                sx={{
-                    marginLeft: '20%',
-                    marginRight: '20%',
-                }}
-            >
-                {isLoggedIn && <CreatePost setPosts={setPosts}/>}
-                <Divider/>
-                <FilterPosts initPosts={posts} setPage={setPage} setPosts={setPosts}/>
-                {
-                    posts && posts.map((post: PostUser) => {
-
-                        return (<Grid item
-                                      sx={{
-                                          my: 1,
-                                          // mx: 'auto',
-                                      }}
-                        >
-                            <ViewPost posts={posts} setPosts={setPosts} post={post}/>
-                        </Grid>)
-                    })
-                }
-
-                <Button
-                    onClick={async () => {
-                        await set_posts()
-                    }}
-                >Load more</Button>
-
-            </Grid>
-
-        );
+    if (res && res.length > 0) {
+      setPosts((pre) => {
+        return posts.length == 0 ? [...res] : [...pre, ...res];
+      });
+    } else if (res && res.length == 0) {
+      enqueueSnackbar("There are no more posts to load.", { variant: "info" });
+    } else {
+      enqueueSnackbar("undefined Error getting posts.", { variant: "error" });
     }
-;
+  }
 
+  useEffect(() => {
+    (async () => {
+      await set_posts();
+    })();
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        margin: "0 auto",
+        padding: 2,
+        maxWidth: "70%",
+      }}
+    >
+      {isLoggedIn && (
+        <Box sx={{ marginBottom: 3 }}>
+          <CreatePost setPosts={setPosts} />
+        </Box>
+      )}
+      <Divider sx={{ marginBottom: 3 }} />
+      <Box sx={{ marginBottom: 3 }}>
+        <FilterPosts initPosts={posts} setPage={setPage} setPosts={setPosts} />
+      </Box>
+      <Grid container spacing={2}>
+        {posts.map((post: PostUser, index) => (
+          <Grid item xs={12} key={index}>
+            <ViewPost posts={posts} setPosts={setPosts} post={post} />
+          </Grid>
+        ))}
+      </Grid>
+      <Box sx={{ textAlign: "center", marginTop: 4 }}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={async () => {
+            await set_posts();
+          }}
+        >
+          Load more
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 export default Discover;
