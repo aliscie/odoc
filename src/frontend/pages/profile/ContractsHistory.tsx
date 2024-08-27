a04d085e41c1a44ae457988aec493c9acf978d9d
koladeolukoya <koladeolukoya@gmail.com>
2024-08-27 07:14:31 +0100

    style: improvement to contract page ui

--- src/frontend/pages/profile/ContractsHistory.tsx
+++ src/frontend/pages/profile/ContractsHistory.tsx
@@ -1,42 +1,72 @@
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import List from "@mui/material/List";
-import * as React from "react";
 import ListItem from "@mui/material/ListItem";
 import Button from "@mui/material/Button";
-import {Principal} from "@dfinity/principal";
-import {CustomContractComponent} from "../../components/ContractTable";
-import {custom_contract, randomString} from "../../DataProcessing/dataSamples";
-import {handleRedux} from "../../redux/store/handleRedux";
-import {CustomContract} from "../../../declarations/backend/backend.did";
-
+import { Principal } from "@dfinity/principal";
+import { CustomContractComponent } from "../../components/ContractTable";
+import {
+  custom_contract,
+  randomString,
+} from "../../DataProcessing/dataSamples";
+import { handleRedux } from "../../redux/store/handleRedux";
+import { CustomContract } from "../../../declarations/backend/backend.did";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
+import * as React from "react";
 
 function ContractsHistory(props: any) {
-    const dispatch = useDispatch();
-    const {contracts, profile} = useSelector((state: any) => state.filesState);
-    const handleClick = () => {
-        let newContract = {
-            ...custom_contract,
-            id:randomString(),
-            creator: profile && Principal.fromText(profile.id),
-            date_created: Date.now() * 1e6
-        }
-        dispatch(handleRedux("ADD_CONTRACT", {contract: newContract}));
-    }
-    return (
-        <List dense>
-            <Button
-                onClick={handleClick}
-                variant={'outlined'}
-            >Create new contract</Button>
-            {
-                Object.values(contracts).map((contract: CustomContract | any) => {
-                    if (contract) {
-                        return <ListItem><CustomContractComponent contract={contract}/></ListItem>
-                    }
-                })
-            }
-        </List>
-    );
+  const dispatch = useDispatch();
+  const { contracts, profile } = useSelector((state: any) => state.filesState);
+
+  const handleClick = () => {
+    let newContract = {
+      ...custom_contract,
+      id: randomString(),
+      creator: profile && Principal.fromText(profile.id),
+      date_created: Date.now() * 1e6,
+    };
+    dispatch(handleRedux("ADD_CONTRACT", { contract: newContract }));
+  };
+
+  return (
+    <Box sx={{ padding: 3, margin: 2 }}>
+      <Typography variant="h5" gutterBottom>
+        Contracts History
+      </Typography>
+      <Button
+        onClick={handleClick}
+        variant="contained"
+        color="primary"
+        sx={{ mb: 2 }}
+      >
+        Create New Contract
+      </Button>
+      <Divider sx={{ mb: 2 }} />
+      <List dense>
+        {Object.values(contracts).map((contract: CustomContract | any) => {
+          if (contract) {
+            return (
+              <ListItem key={contract.id} sx={{ padding: 0, marginBottom: 2 }}>
+                <Paper
+                  elevation={3}
+                  sx={{
+                    width: "100%",
+                    "&:hover": {
+                      boxShadow: 6,
+                    },
+                  }}
+                >
+                  <CustomContractComponent contract={contract} />
+                </Paper>
+              </ListItem>
+            );
+          }
+        })}
+      </List>
+    </Box>
+  );
 }
 
-export default ContractsHistory
+export default ContractsHistory;
