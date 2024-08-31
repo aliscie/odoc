69516dc884517534c08b21f3a870e84fe6d352d7
koladeolukoya <koladeolukoya@gmail.com>
2024-08-31 17:43:16 +0100

    depost test working

--- src/frontend/tests/React/backendMocks.ts
+++ src/frontend/tests/React/backendMocks.ts
@@ -2,4 +2,12 @@ export const backendMocks = {
   delete_payment: vi.fn((id: string) => {
     return { Ok: {} };
   }),
+
+  deposit_usdt: vi.fn(async (amount: number) => {
+    if (amount === 100) {
+      return { Ok: 100 }; // Adjust based on the expected result structure
+    }
+
+    return { Err: "Deposit failed" };
+  }),
 };
