95d63c4d4e4e4ced7742a5520cf2a2a85860b9df
koladeolukoya <koladeolukoya@gmail.com>
2024-09-03 11:13:58 +0100

    fix: coloe issue with app solved

--- src/frontend/ThemeProvider.tsx
+++ src/frontend/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 import { useSelector } from "react-redux";
-import { ThemeProvider as MuiThemeProvider, Theme } from "@mui/material/styles";
+import { ThemeProvider as MuiThemeProvider, CssBaseline } from "@mui/material";
 import { createTheme } from "./theme";
 
 interface ThemeProviderProps {
@@ -9,9 +9,14 @@ interface ThemeProviderProps {
 
 const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
   const { isDarkMode } = useSelector((state: any) => state.uiState);
-  const theme: Theme = createTheme(isDarkMode);
+  const theme = createTheme(isDarkMode);
 
-  return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </MuiThemeProvider>
+  );
 };
 
 export default ThemeProvider;
