c4534cea2635466201bff342107483cba9068783
aliscie <alihushamsci@icloud.com>
2024-09-05 11:14:37 +0800

    unit test in storybook

--- src/frontend/tests/stories/contract/UsetPageTesting.stories.ts
+++ src/frontend/tests/stories/contract/UsetPageTesting.stories.ts
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import { Contract } from "./Contract";
+import { UsetPageTesting } from "./UsetPageTesting";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Example/Button",
-  component: Contract,
+  title: "Example/UserPage",
+  component: UsetPageTesting,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
@@ -18,7 +18,7 @@ const meta = {
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof UsetPageTesting>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
