4477cd219cceb5ca9f89fc7020a40dc0364352b2
koladeolukoya <koladeolukoya@gmail.com>
2024-09-08 06:20:26 +0100

    before merge with ali

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
     threads: false,
     watch: true,
     setupFiles:
+      /* This line of code is a conditional statement that checks if the value of the environment variable
+   `VITE_TEST_ENV` is equal to "backend". If the condition is true, it includes the file
+   "./setup_tests.ts" in the setupFiles array. If the condition is false, it includes an empty array
+   in the setupFiles array. */
       process.env.VITE_TEST_ENV === "backend" ? "./setup_tests.ts" : [],
     setupFilesAfterEnv: [
       "@testing-library/jest-dom/extend-expect",
