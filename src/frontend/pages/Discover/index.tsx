7bb00a50ec7599bd5fd059e981a3ecca0cc44b9b
aliscie <alihushamsci@icloud.com>
2024-12-22 16:28:08 +0800

    refactor: Remove commented-out code and adjust SearchField styling

--- src/frontend/pages/Discover/index.tsx
+++ src/frontend/pages/Discover/index.tsx
@@ -8,101 +8,7 @@ import { useSnackbar } from "notistack";
 import FilterPosts from "./PostsFilters";
 import ViewPost from "./ViewUpdatePost";
 import { useBackendContext } from "../../contexts/BackendContext";
-//
-// const Discover = () => {
-//   const { backendActor } = useBackendContext();
-//   const { searchValue } = useSelector((state: any) => state.uiState);
-//   const { isLoggedIn } = useSelector((state: any) => state.uiState);
-//
-//   const [posts, setPosts] = React.useState<Array<PostUser>>([]); //TODO use redux for this
-//
-//   const [current_page, setPage] = React.useState<number>(0);
-//   const { enqueueSnackbar } = useSnackbar();
-//   useEffect(() => {
-//     let timeoutId: NodeJS.Timeout;
-//
-//     const delayedSearch = async () => {
-//       // TODO later add a Button for deep search_popper in cuz query can cost cycles.
-//       if (searchValue.length > 0) {
-//         let res: Array<PostUser> = await backendActor.search_posts(searchValue);
-//         res && setPosts(res);
-//       } else {
-//         setPage(0);
-//         setPosts([]);
-//         await set_posts();
-//       }
-//     };
-//
-//     // Clear the previous timeout
-//     clearTimeout(timeoutId);
-//
-//     // Set a new timeout
-//     timeoutId = setTimeout(delayedSearch, 300);
-//
-//     // Cleanup function
-//     return () => clearTimeout(timeoutId);
-//   }, [searchValue]);
-//
-//   async function set_posts() {
-//     posts.length > 0 && setPage(posts.length);
-//     let res: Array<PostUser> = await backendActor.get_posts(
-//       BigInt(current_page),
-//       BigInt(current_page + 10),
-//     );
-//
-//     if (res && res.length > 0) {
-//       setPosts((pre) => {
-//         return posts.length == 0 ? [...res] : [...pre, ...res];
-//       });
-//     } else if (res && res.length == 0) {
-//       enqueueSnackbar("There are no more posts to load.", { variant: "info" });
-//     } else {
-//       enqueueSnackbar("undefined Error getting posts.", { variant: "error" });
-//     }
-//   }
-//
-//   useEffect(() => {
-//     (async () => {
-//       await set_posts();
-//     })();
-//   }, []);
-//
-//   return (
-//     <Grid
-//       sx={{
-//         marginLeft: "20%",
-//         marginRight: "20%",
-//       }}
-//     >
-//       {isLoggedIn && <CreatePost setPosts={setPosts} />}
-//       <Divider />
-//       <FilterPosts initPosts={posts} setPage={setPage} setPosts={setPosts} />
-//       {posts &&
-//         posts.map((post: PostUser) => {
-//           return (
-//             <Grid
-//               item
-//               sx={{
-//                 my: 1,
-//                 // mx: 'auto',
-//               }}
-//             >
-//               <ViewPost setPosts={setPosts} post={post} />
-//             </Grid>
-//           );
-//         })}
-//
-//       <Button
-//         onClick={async () => {
-//           await set_posts();
-//         }}
-//       >
-//         Load more
-//       </Button>
-//     </Grid>
-//   );
-// };
-// export default Discover;
+
 
 import {
   Favorite as HeartIcon,
@@ -270,7 +176,7 @@ const Comment = ({ comment, onReply, level = 0 }) => {
 
 const SearchField = ({ searchQuery, setSearchQuery }) => {
   return (
-    <Card sx={{ mb: 3 }}>
+    <Card >
       <CardContent>
         <TextField
           fullWidth
